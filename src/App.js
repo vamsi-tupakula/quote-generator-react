9552580ca512532ac7b2e5ee62b24b0fef0fb2dc
Vamsi <vamsitupakula474@gmail.com>
2022-07-15 08:50:40 +0530

    app done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { useState } from 'react';
 
 function App() {
+  const [quote, setQuote] = useState('Click the Get Next Button to generate Quote')
+
+  const copyText = () => {
+    navigator.clipboard.writeText(quote);
+  }
+
+  const nextQuote = async () => {
+    const apiURL = 'https://api.quotable.io/random'
+    await fetch(apiURL)
+      .then(res => res.json())
+      .then(data => {
+        setQuote(data.content);
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="heading">Quote of the Day</div>
+        <div className="quote">" {quote} "</div>
+        <div className="btns">
+          <Button variant="outlined" color="primary" size="large" onClick={copyText}>Copy &nbsp; <ContentCopyIcon /></Button>
+          <Button variant="contained" color="primary" size="large" onClick={nextQuote}>Get Next</Button>
+        </div>
+      </div>
     </div>
   );
 }
